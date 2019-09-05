ae0df5d6b6aabf1b738e1dc92db4950fdacb82a9
Maurice <m4rsbars@gmail.com>
2019-09-05 11:39:42 +0100

    release build

--- mBetterBag/Main.cs
+++ mBetterBag/Main.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Configuration;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -33,41 +34,49 @@ public class Main : wManager.Plugin.IPlugin
         bagCheckTimer.Elapsed += BagCheckTimer_Elapsed;
         bagCheckTimer.Interval = 5000;
         bagCheckTimer.Start();
-        foreach (var item in Bag.GetBagItem())
-        {
-            Logging.Write(item.GetItemInfo.ItemType);
-        }
+        //foreach (var item in Bag.GetBagItem())
+        //{
+        //    Logging.Write(item.GetItemInfo.ItemType);
+        //}
+        //Debugger.Launch();
     }
 
     private void BagCheckTimer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
     {
         equippedBags.Clear();
-        for (int i = 0; i <= 4; i++)
+        for (int i = 1; i <= 4; i++)
         {
             equippedBags.Add(GetEquippedBagInfo(i));
         }
-        foreach (var bag in equippedBags.Where(x => x.maxSlots > 0))
-        {
-            Logging.Write($"Slot:{bag.bagSlotId}, Invid:{bag.inventoryId}, FreeSlots:{bag.freeSlots}, MaxSlots:{bag.maxSlots}");
-        }
+        //foreach (var bag in equippedBags.Where(x => x.maxSlots > 0))
+        //{
+        //    Logging.Write($"Slot:{bag.bagSlotId}, Invid:{bag.inventoryId}, FreeSlots:{bag.freeSlots}, MaxSlots:{bag.maxSlots}");
+        //}
         foreach (var bag in Bag.GetBagItem().Where(x => x.GetItemInfo.ItemType == "Container"))
         {
-            if (equippedBags.Count(x => x.maxSlots > 0) < 5)
+            Logging.Write($"BetterBag: test {bag.Name}.");
+            if (equippedBags.Count(x => x.maxSlots > 0) < 4)
             {
                 Lua.LuaDoString("ClearCursor()");
-                //ItemsManager.UseItem((uint)bag.Entry);
+                Logging.Write($"BetterBag: Equipping {bag.Name}.");
                 ItemsManager.EquipItemByName(bag.Name);
             }
-            else
+            else if (mBetterBagSettings.CurrentSetting.ReplaceBags && equippedBags.Count(x => x.maxSlots > 0) == 4)
             {
+                Logging.Write("else..");
                 var smallestEquipedBag = equippedBags.Where(y => y.bagSlotId != 0).OrderBy(x => x.maxSlots).First();
-                if (GetContainerNumSlots(bag.Entry) > smallestEquipedBag.maxSlots)
+                Logging.Write(smallestEquipedBag.bagSlotId.ToString() + "_" + smallestEquipedBag.inventoryId.ToString());
+                if (GetInventoryBagSlotCount(bag) > smallestEquipedBag.maxSlots)
                 {
+                    Logging.Write($"BetterBag: {bag.Name} has more slots than {smallestEquipedBag.inventoryId}.");
                     bool moved = MoveItemsToFreeBagFrom(smallestEquipedBag.bagSlotId);
-                    if (Bag.GetBagItem().Where(x => x.ContainedIn == smallestEquipedBag.bagSlotId).Count() == 0 && moved)
+                    Logging.Write(moved.ToString());
+                    if (Bag.GetBagItem().Where(x => (ulong)Bag.GetItemContainerBagIdAndSlot(x.Entry)[0] == smallestEquipedBag.bagSlotId).Count() == 0 && moved)
                     {
+                        var bagidandslot = Bag.GetItemContainerBagIdAndSlot(bag.Entry);
                         Lua.LuaDoString("ClearCursor()");
                         Lua.LuaDoString($"PickupBagFromSlot({smallestEquipedBag.inventoryId})");
+                        Lua.LuaDoString($"PickupContainerItem({bagidandslot[0]}, {bagidandslot[1]})");
                         Bag.PickupContainerItem(bag.Entry);
                     }
                 }
@@ -85,25 +94,49 @@ public class Main : wManager.Plugin.IPlugin
         return Lua.LuaDoString<int>($"return ContainerIDToInventoryID({bagid})");
     }
 
+    public int GetInventoryBagSlotCount(WoWItem bag)
+    {
+        var bagidandslot = Bag.GetItemContainerBagIdAndSlot(bag.Entry);
+        var slotcount = Lua.LuaDoString<int>($@"local tip = mBetterBagTooltip or CreateFrame(""GAMETOOLTIP"", ""mBetterBagTooltip"")
+                                        local L = L or tip: CreateFontString()
+                                        local R = R or tip: CreateFontString()
+                                        L: SetFontObject(GameFontNormal)
+                                        R: SetFontObject(GameFontNormal)
+                                        tip: AddFontStrings(L, R)
+                                        tip: SetOwner(WorldFrame, ""ANCHOR_NONE"")
+                                        local _, _, itemlink, itemid = string.find(GetContainerItemLink({bagidandslot[0]}, {bagidandslot[1]}), '|c%x+|H(item:(%d+):%d+:%d+:%d+)|h%[.-%]|h|r');
+                                        tip: SetHyperlink(itemlink)
+                                        for i = 1, tip:NumLines() do
+                                            tmp = getglobal(""mBetterBagTooltipTextLeft""..i);
+                                            if (tmp) then
+                                              if string.match(tmp: GetText(), ""Slot Bag"") then
+                                                return string.match(tmp: GetText(), ""(.*) Slot Bag"")
+                                              end
+                                            end
+                                        end");
+        return slotcount;
+    }
+
     public bool MoveItemsToFreeBagFrom(ulong oldbagid)
     {
         //foreach item in the bag we're replacing, move it to another bag 
-        foreach (WoWItem item in Bag.GetBagItem().Where(x => x.ContainedIn == oldbagid))
+        var items = Bag.GetBagItem();
+        foreach (var item in items.Where(x => (ulong)Bag.GetItemContainerBagIdAndSlot(x.Entry)[0] == oldbagid))
         {
-
-            var bagWithFreeSlots = equippedBags.Where(x => x.freeSlots > 0 && x.bagSlotId != oldbagid);
-            if (bagWithFreeSlots.Count() > 0)
+            var bagWithFreeSlots = equippedBags.Where(x => x.freeSlots > 0 && x.bagSlotId != oldbagid).First();
+            var bagidandslot = Bag.GetItemContainerBagIdAndSlot(item.Entry);
+            if (bagWithFreeSlots != null)
             {
                 Lua.LuaDoString("ClearCursor()");
                 Logging.Write($"BetterBag: Moving {item.Name} to a better bag.");
-                Bag.PickupContainerItem(Bag.GetItemContainerBagIdAndSlot(item.Entry));
-                if (bagWithFreeSlots.Single().bagSlotId == 0)
+                Lua.LuaDoString($"PickupContainerItem({bagidandslot[0]}, {bagidandslot[1]})");
+                if (bagWithFreeSlots.bagSlotId == 0)
                 {
                     Lua.LuaDoString("PutItemInBackpack()");
                 }
                 else
                 {
-                    Lua.LuaDoString($"PutItemInBag({bagWithFreeSlots.Single().inventoryId})");
+                    Lua.LuaDoString($"PutItemInBag({bagWithFreeSlots.inventoryId})");
                 }
             }
             else
@@ -145,16 +178,21 @@ public class Main : wManager.Plugin.IPlugin
 [Serializable]
 public class mBetterBagSettings : Settings
 {
-
     [Setting]
     [Category("Settings")]
-    [DisplayName("Number")]
-    [Description("Description")]
+    [DisplayName("Replace existing?")]
+    [Description("Will try to replace existing bags with better bags.")]
     public bool ReplaceBags { get; set; }
+
+    //[Setting]
+    //[Category("Settings")]
+    //[DisplayName("Replace existing?")]
+    //[Description("Will try to replace existing bags with better bags.")]
+    //public bool ReplaceBags { get; set; }
 
     private mBetterBagSettings()
     {
-        ReplaceBags = true;
+        ReplaceBags = false;
     }
 
     public static mBetterBagSettings CurrentSetting { get; set; }
--- mFashion/Main.cs
+++ mFashion/Main.cs
@@ -31,7 +31,72 @@ public class Main : wManager.Plugin.IPlugin
     private void FashionCheck_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
     {
         var equippedItems = EquippedItems.GetEquippedItems();
-        //equippedItems.First().GetItemInfo.ItemEquipLoc
+        foreach (var i in Bag.GetBagItem().Where(i => i.GetItemInfo.ItemEquipLoc.Contains("INVTYPE")))
+        {
+
+            var ii = i.GetItemInfo;
+            if (ii.ItemEquipLoc == "") continue;
+            if (ii.ItemEquipLoc == "INVTYPE_BAG") continue;
+            SkillLine skill;
+            var parsed = Enum.TryParse(ii.ItemSubType, out skill);
+            if ((ii.ItemType=="Armor" || ii.ItemType=="Weapon") && parsed ? !Skill.Has(skill) : false)
+            {
+                Logging.Write($"Dont have skill for {i.Name}");
+                continue;
+            }
+            if (_weaponSlots.Any(s => s == ii.ItemEquipLoc)) continue;
+            var x = equippedItems.Where(g => g.GetItemInfo.ItemEquipLoc == ii.ItemEquipLoc);
+            // Weapon logic
+            //if( _weaponSlots.Any(s=>s == ii.ItemEquipLoc))
+            //{
+            //    if(ii.ItemEquipLoc == "INVTYPE_RANGED"|| ii.ItemEquipLoc == "INVTYPE_THROWN")
+            //    {
+            //        // ranged
+            //        Logging.Write("i.Type+", "+i.GetItemInfo.ItemType+", "+i.GetItemInfo.ItemSubType);
+            //    }
+            //    else
+            //    {
+            //        // main
+
+            //    }
+            //}
+
+            if (x.Count() >= 2)
+            {
+                foreach(var y in x)
+                {
+                    Logging.Write($"More than 1 {y.Name}");
+                }                
+                continue;
+            }
+            else if (x.Count() == 1)
+            {
+                if (ii.ItemLevel > x.First().GetItemInfo.ItemLevel && ii.ItemRarity >= x.First().GetItemInfo.ItemRarity && ii.ItemMinLevel <= ObjectManager.Me.Level)
+                {
+                    
+                    
+                        Logging.Write($"Equipping {x.First().Name} because it's better");
+                        ItemsManager.EquipItemByName(i.Name);
+                                                            
+                }
+            }
+            else if (x.Count() == 0)
+            {
+                if ((ii.ItemEquipLoc == "INVTYPE_SHIELD" || ii.ItemEquipLoc == "INVTYPE_HOLDABLE") && x.Any(h => h.GetItemInfo.ItemEquipLoc == "INVTYPE_2HWEAPON"))
+                {
+                    continue;                    
+                }
+                else if ((ii.ItemEquipLoc == "INVTYPE_CHEST" || ii.ItemEquipLoc == "INVTYPE_ROBE") && x.Any(h=>h.GetItemInfo.ItemEquipLoc == "INVTYPE_CHEST" || h.GetItemInfo.ItemEquipLoc == "INVTYPE_ROBE"))
+                {
+                    continue;
+                }
+                else if (ii.ItemMinLevel <= ObjectManager.Me.Level)
+                {
+                    Logging.Write($"Equipping {i.Name} because we had nothing of that type");
+                    ItemsManager.EquipItemByName(i.Name);
+                }
+            }
+        }
     }
 
     public void Settings()
@@ -42,53 +107,70 @@ public class Main : wManager.Plugin.IPlugin
         mFashionSettings.CurrentSetting.Save();
     }
 
-    [Serializable]
-    public class mFashionSettings : Settings
+    private static List<string> _weaponSlots = new List<string>
     {
+        "INVTYPE_2HWEAPON",
+        "INVTYPE_HOLDABLE",
+        "INVTYPE_RANGED",
+        "INVTYPE_THROWN",
+        "INVTYPE_WEAPON",
+        "INVTYPE_WEAPONMAINHAND",
+        "INVTYPE_WEAPONOFFHAND",
+        "INVTYPE_SHIELD"
+    };
+}
+[Serializable]
+public class mFashionSettings : Settings
+{
 
-        [Setting]
-        [Category("Settings")]
-        [DisplayName("Equip rares")]
-        [Description("When enabled the bot will equip rare quality items (blues)")]
-        public bool EquipRares { get; set; }
+    [Setting]
+    [Category("Settings")]
+    [DisplayName("Equip rares")]
+    [Description("When enabled the bot will equip rare quality items (blues)")]
+    public bool EquipRares { get; set; }
+    [Setting]
+    [Category("Settings")]
+    [DisplayName("Ranged equip type")]
+    [Description("Set what type of ranged weapon you want to auto equip")]
+    [MarsSettingsGUI.DropdownList(new string[] { "Wands", "Throwables", "Bows", "Guns" })]
+    public short RangedType { get; set; }
 
-        private mFashionSettings()
+    private mFashionSettings()
+    {
+        EquipRares = true;
+    }
+
+    public static mFashionSettings CurrentSetting { get; set; }
+
+    public bool Save()
+    {
+        try
         {
-            EquipRares = true;
+            return Save(AdviserFilePathAndName("mFashion", ObjectManager.Me.Name + "." + Usefuls.RealmName));
         }
-
-        public static mFashionSettings CurrentSetting { get; set; }
-
-        public bool Save()
+        catch (Exception e)
         {
-            try
-            {
-                return Save(AdviserFilePathAndName("mFashion", ObjectManager.Me.Name + "." + Usefuls.RealmName));
-            }
-            catch (Exception e)
-            {
-                Logging.WriteError("mFashionSettings > Save(): " + e);
-                return false;
-            }
-        }
-
-        public static bool Load()
-        {
-            try
-            {
-                if (File.Exists(AdviserFilePathAndName("mFashion", ObjectManager.Me.Name + "." + Usefuls.RealmName)))
-                {
-                    CurrentSetting =
-                        Load<mFashionSettings>(AdviserFilePathAndName("mFashion", ObjectManager.Me.Name + "." + Usefuls.RealmName));
-                    return true;
-                }
-                CurrentSetting = new mFashionSettings();
-            }
-            catch (Exception e)
-            {
-                Logging.WriteError("mFashionSettings > Load(): " + e);
-            }
+            Logging.WriteError("mFashionSettings > Save(): " + e);
             return false;
         }
+    }
+
+    public static bool Load()
+    {
+        try
+        {
+            if (File.Exists(AdviserFilePathAndName("mFashion", ObjectManager.Me.Name + "." + Usefuls.RealmName)))
+            {
+                CurrentSetting =
+                    Load<mFashionSettings>(AdviserFilePathAndName("mFashion", ObjectManager.Me.Name + "." + Usefuls.RealmName));
+                return true;
+            }
+            CurrentSetting = new mFashionSettings();
+        }
+        catch (Exception e)
+        {
+            Logging.WriteError("mFashionSettings > Load(): " + e);
+        }
+        return false;
     }
 }
